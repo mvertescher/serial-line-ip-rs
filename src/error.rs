ef4bcdfa39892ffb33978660996352e38c9ac0f8
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2021-03-25 17:20:55 +0100

    Implement `core::fmt::Display` for `Error`

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 //! Possible SLIP encoding and decoding errors
 
+use core::fmt;
+
 /// Type alias for handling SLIP errors.
 pub type Result<T> = core::result::Result<T, self::Error>;
 
@@ -17,4 +19,15 @@ pub enum Error {
     BadHeaderDecode,
     /// The decoder cannot process the SLIP escape sequence.
     BadEscapeSequenceDecode,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match self {
+            Error::NoOutputSpaceForHeader => "insufficient space in output buffer for header",
+            Error::NoOutputSpaceForEndByte => "insufficient space in output buffer for end byte",
+            Error::BadHeaderDecode => "malformed header",
+            Error::BadEscapeSequenceDecode => "malformed escape sequence",
+        })
+    }
 }
